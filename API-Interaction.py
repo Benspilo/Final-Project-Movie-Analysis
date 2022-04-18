ef5a3fd5913cd4c2e477e716a246d73f355f3380
unknown <benspilo@umich.edu>
2022-04-17 21:34:45 -0400

    api interaction all done

--- API-Interaction.py
+++ API-Interaction.py
@@ -4,7 +4,7 @@ import os
 import requests
 import bs4
 import re
-
+import locale
 
 #IMDB API key: k_2rg9hfrx
 
@@ -41,67 +41,95 @@ def setup_IMDB_table(cur, conn):
 def setup_wiki_table(cur, conn):
     cur.execute("CREATE TABLE IF NOT EXISTS Wiki_data (Name TEXT PRIMARY KEY, date TEXT, Budget TEXT, Box_office TEXT, Length TEXT)")
 
-def get_wiki(wiki_html):
+def get_wiki(film_id):
+
+    wiki_html = "https://www.boxofficemojo.com/title/" + film_id
     url = wiki_html
     resp = requests.get(url)
     soup = bs4.BeautifulSoup(resp.content, "html.parser")
-    table = soup.find('table', class_ = "infobox vevent")
+    table = soup.find('div', class_ = "a-section a-spacing-none mojo-summary-values mojo-hidden-from-mobile")
     
     info = {}
 
-    tag = table.find(text=re.compile(r'Budget'))
-    budget = tag.parent.parent
-    
-    tag = table.find(text=re.compile(r'Box office'))
-    box = tag.parent.parent
-
-    tag = table.find(text=re.compile(r'Release date'))
-    date = tag.parent.parent.parent
     try:
-        info["Month"] = date.find('ul').text
+        tag = table.find(text=re.compile(r'Budget'))
+        budget = tag.parent.parent
+        budget_text = budget.find('span', class_ = "money").text
     except:
-        info["Month"] = date.find('th').text
-    
-    tag = table.find(text=re.compile(r'Running time'))
-    time = tag.parent.parent.parent
+        budget_text = "00"
 
+    try:
+        #tag = soup.find(text=re.compile(r'Original Release'))
+        tag = soup.find('tr', class_ = 'mojo-revenue-source-theatrical')
+        box = tag
+        box_text = box.find('span', class_ = "money").text
+    except:
+        box_text = "00"
+
+    try:
+        tag = table.find(text=re.compile(r'Earliest Release Date'))
+        date = tag.parent.parent
+        month_1 = date.find_all('span')
+        month_text = re.search("span>(\w+)", str(month_1[1]))[1]
+    except:
+        month_text = "NA"
+
+
+    tag = table.find(text=re.compile(r'Running Time'))
+    time = tag.parent.parent    
+    time_1 = time.find_all('span')
+    time_text = re.search(("span>(.+)</"), str(time_1[1]))[0]
     
-    info["Budget"] = budget.find('td').text
-    info["Box Office"] = box.find('td').text
-    
-    info["Length"] = time.find('td').text
+
+    info["Month"] = re.search('\w+', month_text)[0]
+    info["Budget"] = clean_num(budget_text[1:])
+    info["Box Office"] = clean_num(box_text[1:])
+    info["Length"] = convert_time(time_text)  #re.search('\', time_text)[0]
 
     return info
     
-def get_wiki_link(input_id):
-    link = 'https://imdb-api.com/en/API/Wikipedia/' + use_key + '/' + input_id
-    data = requests.get(link)
-    wiki_info = json.loads(data.text)
+def clean_num(number):
+    locale.setlocale(locale.LC_ALL, "")
+    return locale.atoi(number)
 
-    wiki_html = wiki_info['url']
-
-    return get_wiki(wiki_html)
+def convert_time(time):
+    try:
+        hours = re.search('(\d+) hr', time)[1]
+    except:
+        hours = '0'
+    try:
+        mins = re.search('(\d+) min', time)[1]
+    except:
+        mins = '0'
+    hournum = int(str(hours))* 60
+    total = hournum + int(str(mins))
+    return total
     
 
 #regex \b\w* (month)
-#regex \d* million|\d* thousand (box office and budget)
+#regex (\d*) (million)|(\d*) (thousand) (box office and budget)
 
 def build_db(cur, conn):
     link = 'https://imdb-api.com/API/Top250Movies/' + use_key + '/'
     data = requests.get(link)
     movie_dict = json.loads(data.text)
     
-
-    #cur.execute("SELECT MAX(Rank) FROM IMDB_data")
-    #SELECT UNIQUE Rank 
+    try:
+        cur.execute("SELECT MAX(Rank) FROM IMDB_data")
+        max_rank = int(str(cur.fetchall()[0][0]))
+    except:
+        max_rank = 1
 
     for i in movie_dict['items']:
+
         name = i['title']
         id = i['id']
         rank = i['rank']
         year = i['year']
         rating = i['imDbRating']
 
+        if int(rank) < int(max_rank):
+            continue
         
         cur.execute("""
         INSERT OR IGNORE INTO IMDB_data (Name, id, Rank, Year, Rating)
@@ -111,7 +139,7 @@ def build_db(cur, conn):
 
         conn.commit()
         
-        wiki_values = get_wiki_link(id)
+        wiki_values = get_wiki(id)
 
         date = wiki_values["Month"]
         budget = wiki_values["Budget"]
@@ -126,11 +154,14 @@ def build_db(cur, conn):
         (name, date, budget, box_office, length))
         
         conn.commit()
+
+        if int(rank) >= max_rank + 25:
+            break
+        print(rank)
         
         #LIMIT 25
 
-
-def main():
+def realtest():
     cur, conn = setup_database('IMDB-data.db')
     setup_IMDB_table(cur, conn)
     setup_wiki_table(cur, conn)
@@ -138,9 +169,12 @@ def main():
     #repeat 10 times, to get all 250 datapoints
     build_db(cur, conn)
 
-    print("all done!!!\n")
-    
-    """"
-    wiki_data = get_wiki()
-    """
+def faketest():
+    print(get_wiki("tt0325980"))
+
+
+
+def main():
+    realtest()
+    #faketest()
 main()
--- Graphs-analysis.py
+++ Graphs-analysis.py
@@ -1,1 +1,25 @@
-print("hello world")
+# box office by month 
+# budget to box office 
+# budget per minute
+#
+
+import sqlite3
+import os
+import re
+
+def setup_database(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def box_by_month(cur, conn):
+    cur.execute('''
+    SELECT Wiki_data.box_office, Wiki_data.release_date 
+    FROM Wiki_data 
+    GROUP BY Wiki_data.release_date
+    ''')
+
+
+def main():
+    cur, conn = setup_database('IMDB-data.db')
